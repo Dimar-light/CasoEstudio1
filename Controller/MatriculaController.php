01e1796bfe06dca6f8666cec7d4ba6a204bb2651
Dimar-light <margelinearce1981@gmail.com>
2026-06-24 19:05:20 -0600

    Implementacion 1

---
+++ Controller/MatriculaController.php
@@ -0,0 +1,22 @@
+<?php
+include_once $_SERVER['DOCUMENT_ROOT'] . '/CasoEstudio1/Model/MatriculaModel.php';
+
+$mensajeAlerta = "";
+
+if (isset($_POST["btnRegistrarMatricula"])) {
+    $identificacion = $_POST["txtIdentificacion"];
+    $monto = $_POST["numMonto"];
+    $tipoCurso = $_POST["txtTipoCurso"];
+
+    $respuesta = RegistrarMatriculaModel($identificacion, $monto, $tipoCurso);
+
+    if ($respuesta === true) {
+        header("Location: /CasoEstudio1/View/ConsultaMatricula.php");
+        exit();
+    } else {
+        $mensajeAlerta = $respuesta;
+    }
+}
+
+$listaMatriculas = ConsultarMatriculasModel();
+?>
---
+++ Model/MatriculaModel.php
@@ -0,0 +1,39 @@
+<?php
+include_once $_SERVER['DOCUMENT_ROOT'] . '/CasoEstudio1/Model/ConexionModel.php';
+
+function RegistrarMatriculaModel($identificacion, $monto, $tipoCurso)
+{
+    try {
+        $conn = OpenDB();
+        $stmt = $conn->prepare("CALL spRegistrarMatricula(?, ?, ?)");
+        $stmt->bind_param("sdi", $identificacion, $monto, $tipoCurso);
+        
+        $result = $stmt->execute();
+        $stmt->close();
+        CloseDB($conn);
+        return $result;
+    } catch (Exception $e) {
+        return $e->getMessage();
+    }
+}
+
+function ConsultarMatriculasModel()
+{
+    try {
+        $conn = OpenDB();
+        $sql = "CALL spConsultarMatriculas()";
+        $response = $conn->query($sql);
+
+        $datos = array();
+        if ($response) {
+            while ($fila = $response->fetch_assoc()) {
+                $datos[] = $fila;
+            }
+        }
+        CloseDB($conn);
+        return $datos;
+    } catch (Exception $e) {
+        return array();
+    }
+}
+?>
